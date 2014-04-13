a6feeff8aa9b0d6eb72328407936a15e7837bc0f
Brayden Cleary <brayden.cleary@gmail.com>
2014-04-13 11:10:25 -0700

    Initial Commit

---
+++ homework_2_2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  homework_2_2
+//
+//  Created by Brayden Cleary on 4/13/14.
+//  Copyright (c) 2014 braydencleary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ homework_2_2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  homework_2_2
+//
+//  Created by Brayden Cleary on 4/13/14.
+//  Copyright (c) 2014 braydencleary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
